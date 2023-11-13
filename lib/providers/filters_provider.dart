4d31793c8e42d0406d5ecdfdc1ccfb8a7c2aa086
voidchef <neelutpal.saha@s.amity.edu>
2023-11-13 22:24:39 +0530

    feat: use riverpod for state management

---
+++ lib/providers/filters_provider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meals_app/providers/meals_provider.dart';
+
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
+class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  FiltersNotifier()
+      : super({
+          Filter.glutenFree: false,
+          Filter.lactoseFree: false,
+          Filter.vegetarian: false,
+          Filter.vegan: false,
+        });
+
+  void setFilters(Map<Filter, bool> chosenFilters) {
+    state = chosenFilters;
+  }
+
+  void setFilter(Filter filter, bool isActive) {
+    state = {
+      ...state,
+      filter: isActive,
+    };
+  }
+}
+
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+  (ref) {
+    return FiltersNotifier();
+  },
+);
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final filters = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (filters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (filters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (filters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (filters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/providers/favourites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
+class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
-    required this.onToggleFavorite,
   });
 
   final Meal meal;
-  final void Function(Meal meal) onToggleFavorite;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favouriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavourite = favouriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(title: Text(meal.title), actions: [
         IconButton(
           onPressed: () {
-            onToggleFavorite(meal);
+            final wasAdded = ref
+                .read(favoriteMealsProvider.notifier)
+                .toggleMealFavoriteStatus(meal);
+            ScaffoldMessenger.of(context).clearSnackBars();
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                    wasAdded ? 'Meal added as a favourite.' : 'Meal removed.'),
+              ),
+            );
           },
-          icon: const Icon(Icons.star),
+          icon: Icon(isFavourite ? Icons.star : Icons.star_border),
         )
       ]),
       body: SingleChildScrollView(
